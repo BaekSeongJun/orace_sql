203c8a2169bf3062df693bf01490f44d647dc9ef
BaekSeongJun <por9535@gmail.com>
2026-07-10 17:23:33 +0900

    2026-07-10 spring jpa practice

--- 20springboard_project.sql
+++ 20springboard_project.sql
@@ -21,3 +21,24 @@ SELECT * FROM board;
 SELECT * FROM user_tables;
 SELECT * FROM user_sequences;
 SELECT * FROM user_constraints;
+
+SELECT * FROM board
+ORDER BY regdate desc;
+
+--jpa
+--jpaboard
+
+CREATE TABLE jpaboard(
+    boardNo NUMBER, 
+    title VARCHAR2(200) NOT NULL, 
+    content VARCHAR2(1000), 
+    writer VARCHAR2(100) NOT NULL, 
+    regDate DATE DEFAULT SYSDATE NOT NULL , 
+    CONSTRAINT pk_jpaboard_no PRIMARY KEY(boardNo)
+);
+
+CREATE SEQUENCE jpa_board_seq
+START WITH 1
+INCREMENT BY 1
+NOCYCLE
+CACHE 2;
